13964436621a7cd0f2cb2f1880fe2b7008f381b1
Alvaro De La Torre <alvarodltp@gmail.com>
2018-09-07 11:48:30 -0400

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,13 @@
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+let sorted = []
+for(let i=arr.length - 1; i>=0; i--){
+  for(let j=i; j >= 0; j--){
+    if(arr[i] < arr[j]){
+      let temp = arr[i]
+      arr[i] = arr[j];
+      arr[j] = temp
+    }
+  }
+}
+  return arr
 }
